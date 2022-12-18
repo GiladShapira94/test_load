9bc3d04ffd9226aa916d9acebc3f354a6a401692
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2022-12-18 10:56:50 +0200

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -7,4 +7,5 @@ def fetch_data(context, dataset: mlrun.DataItem,format="csv"):
     df  = dataset.as_df()
     context.logger.info("saving dataframe to s3")
     context.log_dataset("dataset", df=df, format=format, index=False)
+    return "v1"
     
